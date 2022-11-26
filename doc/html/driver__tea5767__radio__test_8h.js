0e9d7970d0feeaedbca9a91d16b4286367363d15
e <lishifenging@outlook.com>
2022-11-26 12:40:22 +0800

    docs: change the new logo

--- doc/html/driver__tea5767__radio__test_8h.js
+++ doc/html/driver__tea5767__radio__test_8h.js
@@ -1,4 +1,4 @@
 var driver__tea5767__radio__test_8h =
 [
-    [ "tea5767_radio_test", "group__tea5767__test__driver.html#ga17d4af16f4a0839fbb0c77e13d7d3bff", null ]
+    [ "tea5767_radio_test", "group__tea5767__test__driver.html#gad5b4becb7b9ddb7a928bdadd104981b2", null ]
 ];
